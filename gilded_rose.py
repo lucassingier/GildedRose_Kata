b2e0b3b6c1c7bed1da4c7b21e24166f19390424c
louis dafflon <louis.dafflon@ynov.com>
2022-04-29 17:06:30 +0200

    code Louis  revision Lucas - Premiere refacto, passage du test à green + simplification sur les switchs

--- gilded_rose.py
+++ gilded_rose.py
@@ -1,6 +1,3 @@
-# -*- coding: utf-8 -*-
-
-test = "test"
 class GildedRose(object):
 
     def __init__(self, items):
@@ -8,20 +5,21 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
+            # item.name == Sulfuras, Hand of Ragnaros, foo
             if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
                 if item.quality > 0:
                     if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
+                        item.quality -= 1
+            # item.name == "Aged Brie" or "Backstage passes to a TAFKAL80ETC concert"
             else:
                 if item.quality < 50:
-                    item.quality = item.quality + 1
+                    item.quality += 1
                     if item.name == "Backstage passes to a TAFKAL80ETC concert":
                         if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
+                            item.quality += 1
                         if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
+                            item.quality += 1
+
             if item.name != "Sulfuras, Hand of Ragnaros":
                 item.sell_in = item.sell_in - 1
             if item.sell_in < 0:
@@ -29,9 +27,9 @@ class GildedRose(object):
                     if item.name != "Backstage passes to a TAFKAL80ETC concert":
                         if item.quality > 0:
                             if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
+                                item.quality -= 1
                     else:
-                        item.quality = item.quality - item.quality
+                        item.quality = 0
                 else:
                     if item.quality < 50:
                         item.quality = item.quality + 1
