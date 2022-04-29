31c0077bc2d7e654c50b66e68619b1cfe08ada64
lucassingier <lucas.singier@gmail.com>
2022-04-30 00:51:40 +0200

    LSI: Update item.sell_in oubli
    Ajout des sell_in manquants

--- gilded_rose.py
+++ gilded_rose.py
@@ -16,6 +16,7 @@ class GildedRose(object):
             if item.name == Age:
                 if item.quality < 50:
                     item.quality += 1
+                item.sell_in -= 1
                 if item.sell_in < 0:
                     if item.quality < 50:
                         item.quality += 1
@@ -27,6 +28,7 @@ class GildedRose(object):
                     item.quality += 1
                     if item.sell_in < 6:
                         item.quality += 1
+                item.sell_in -= 1
                 if item.sell_in < 0:
                     item.quality = 0
 
