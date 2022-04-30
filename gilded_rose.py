4c07ada12a45ba6f51801d42d05c7a7423e886a9
louis dafflon <louis.dafflon@ynov.com>
2022-04-30 15:09:38 +0200

    Louis Ajout de tests pour respecter toutes les specifications de l'énoncé : green

--- gilded_rose.py
+++ gilded_rose.py
@@ -11,36 +11,28 @@ class GildedRose(object):
             if item.name == Sulfuras:
                 item.sell_in = 0 #TO DO Enlever le critère sell_in de Sulfuras car ne se périme pas
             else:
-                item.sell_in -= 1 #Tous les items diminuent leur sell_in de 1
-
-            if item.name == Age:
-                if item.quality < 50:
-                    item.quality += 1
-                item.sell_in -= 1
-                if item.sell_in < 0:
+                item.sell_in -= 1 #Tous les items diminuent leur sell_in de 1 chaque fin de journée
+                if item.name == Age:
                     if item.quality < 50:
                         item.quality += 1
-
-            elif item.name == Backstage:
-                if item.quality < 50:
-                    item.quality += 1.
-                if item.sell_in < 11:
-                    item.quality += 1
-                    if item.sell_in < 6:
+                    if item.sell_in < 0:
+                        if item.quality < 50:
+                            item.quality += 1
+                elif item.name == Backstage:
+                    if item.quality < 50:
+                        item.quality += 1.
+                    if item.sell_in < 11:
                         item.quality += 1
-                item.sell_in -= 1
-                if item.sell_in < 0:
-                    item.quality = 0
-
-            elif item.name == Sulfuras:
-                if item.quality < 50:
-                        item.quality += 1
-            else:
-                if item.quality > 0:
-                    item.quality -= 1
-                if item.sell_in < 0:
+                        if item.sell_in < 6:
+                            item.quality += 1
+                    if item.sell_in < 0:
+                        item.quality = 0
+                else:
                     if item.quality > 0:
-                        item.quality -=1
+                        item.quality -= 1
+                    if item.sell_in < 0:
+                        if item.quality > 0:
+                            item.quality -=1
 
 
 class Item:
--- test_guilded_rose.py
+++ test_guilded_rose.py
@@ -5,50 +5,65 @@ from gilded_rose import Item, GildedRose
 
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-
-        self.assertEquals("foo", items[0].name)
-
-    def test_AgedBrie_plus1(self):
+    #agedbrie
+    def test_AgedBrie_should_increase_quality_one(self):
         items = [Item("Aged Brie", 10, 20)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEquals(21, items[0].quality)
 
-    def test_AgedBrie_plus2(self):
-        items = [Item("Aged Brie", -1, 20)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEquals(22, items[0].quality)
-
-    def test_Backstage(self):
-        items = [Item("Backstage passes to a TAFKAL80ETC concert", 10, 40)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEquals(42, items[0].quality) and self.assertEquals(9, items[0].sell_in)
-
-    def test_Sulfuras(self):
-        items = [Item("Sulfuras, Hand of Ragnaros", 0, 90)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEquals(90, items[0].quality)
-
-    def test_Sulfuras_quality49(self):
-        items = [Item("Sulfuras, Hand of Ragnaros", 1, 49)]
+    def test_AgedBrie_should_never_has_quality_more50(self):
+        items = [Item("Aged Brie", -1, 50)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEquals(50, items[0].quality)
 
-    def test_Sulfuras_sellin_Always_zero(self):
-        items = [Item("Sulfuras, Hand of Ragnaros", 50, 49)]
+    def test_AgedBrie_decrease_sellin(self):
+        items = [Item("Aged Brie", 5, 20)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals(4, items[0].sell_in)
+
+    #backstage
+    def test_Backstage_between_11_and_5_sellin_should_increase_quality_2(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 10, 40)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals(42, items[0].quality)
+
+    def test_Backstage_less_5_sellin_should_increase_quality_3(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 4, 40)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals(43, items[0].quality)
+
+    def test_Backstage_less_0_sellin_should_be_quality_0(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", -1, 40)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals(0, items[0].quality)
+
+    def test_Backstage_decrease_sellin(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 10, 40)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals(9, items[0].sell_in)
+
+    #sulfuras
+    def test_Sulfuras_quality_never_change(self):
+        items = [Item("Sulfuras, Hand of Ragnaros", 1, 80)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals(80, items[0].quality)
+
+    def test_Sulfuras_sellin_always_zero(self):
+        items = [Item("Sulfuras, Hand of Ragnaros", 50, 80)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEquals(0, items[0].sell_in)
 
-    def test_random_item_decrease_one_all(self):
+    #classic item
+    def test_classic_item_decrease_one_all(self):
         items = [Item("Item commun", 10, 10)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
